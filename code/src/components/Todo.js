bf1df54cbf01e4383341f38d2c0f4642371051a1
gaurav8077 <gauravkhandelwal8006@gmail.com>
2024-07-30 23:21:22 +0530

    Marking todos complete, edit and delete are done

--- code/src/components/Todo.js
+++ code/src/components/Todo.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faTrash} from '@fortawesome/free-solid-svg-icons'
-import {faPenToSquare} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = () => {
-    return (
-        <div className='Todo'>
-            <p>Go to Schoool</p>
-            <div>
-                <FontAwesomeIcon icon={faPenToSquare} />
-                <FontAwesomeIcon icon={faTrash} />
-            </div>
+export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+  return (
+    <div className="Todo">
+        <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+        <div>
+        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+        <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
         </div>
-    )
+    </div>
+  )
 }
